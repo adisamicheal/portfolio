f4de96db72a616908bdc2fab5d56ece32eb28ac2
adisamicheal <adisamicheal20@gmail.com>
2023-02-19 23:23:52 +0000

    feat: add google analytics

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { createMetaManager } from 'vue-meta'
+import VueAnalytics from 'vue-analytics';
 
-createApp(App).use(store).use(router).use(createMetaManager()).mount('#app')
+createApp(App).use(store).use(router).use(createMetaManager()).use(VueAnalytics as any, {
+    id: '354369322',
+    router
+}).mount('#app')
